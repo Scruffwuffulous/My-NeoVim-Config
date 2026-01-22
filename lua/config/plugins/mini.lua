af6e8b1741c0c023cf80bfdf706551e075590194
Scruffwuffulous <jtopowerthree@gmail.com>
2026-01-22 22:41:18 +0000

    A few more plugins

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,6 +1,10 @@
 return {
 		{
 				'nvim-mini/mini.nvim',
-				version = '*'
+				version = '*',
+				config = function()
+					local statusline = require 'mini.statusline'
+					statusline.setup {use_icons = true}
+				end
 		}
 }
